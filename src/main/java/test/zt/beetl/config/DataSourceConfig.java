739069da847fedc10b781e743d50bd512d4c6716
臧涛 <zangtao-sj@bestpay.com.cn>
2020-01-03 19:13:21 +0800

    commit

--- src/main/java/test/zt/beetl/config/DataSourceConfig.java
+++ src/main/java/test/zt/beetl/config/DataSourceConfig.java
@@ -1,6 +1,8 @@
 package test.zt.beetl.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.beetl.sql.ext.spring4.BeetlSqlScannerConfigurer;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,15 +16,29 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class DataSourceConfig {
+    /**
+     * 配置数据源
+     *
+     * @param env
+     * @return
+     */
+//    @Bean(name = "datasource")
+//    public DataSource datasource(Environment env) {
+//        DruidDataSource ds = new DruidDataSource();
+//        ds.setUrl(env.getProperty("spring.datasource.url"));
+//        ds.setUsername(env.getProperty("spring.datasource.username"));
+//        ds.setPassword(env.getProperty("spring.datasource.password"));
+//        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+//        return ds;
+//    }
 
-    @Bean(name = "datasource")
-    public DataSource datasource(Environment env) {
-        DruidDataSource ds = new DruidDataSource();
-        ds.setUrl(env.getProperty("spring.datasource.url"));
-        ds.setUsername(env.getProperty("spring.datasource.username"));
-        ds.setPassword(env.getProperty("spring.datasource.password"));
-        ds.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        return ds;
-    }
+//    @Bean(name= "beetlSqlScannerConfigurer")
+//    public BeetlSqlScannerConfigurer getBeetlSqlScannerConfigurer(Environment env){
+//        BeetlSqlScannerConfigurer conf = new BeetlSqlScannerConfigurer();
+//        conf.setBasePackage(env.getProperty("beetlsql.basePackage"));
+//        conf.setDaoSuffix(env.getProperty("beetlsql.daoSuffix"));
+//        conf.setSqlManagerFactoryBeanName("sqlManageFactoryBean");
+//        return conf;
+//    }
 
 }
--- src/main/java/test/zt/beetl/dao/UserDao.java
+++ src/main/java/test/zt/beetl/dao/UserDao.java
@@ -8,7 +8,7 @@ import test.zt.beetl.entity.User;
  * @author zangtao
  * @date 2020/1/2 12:21
  */
-@SqlResource("user.user")
+@SqlResource("user/user")
 public interface UserDao extends BaseMapper<User> {
 
 
